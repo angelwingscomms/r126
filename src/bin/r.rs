b1ad71b7fafc4ada9d8c9e869f8fcdeb4ca034c3
Gold Edem Hogan <edge37@outlook.com>
2025-01-17 21:50:12 +0100

    .

--- src/bin/r.rs
+++ src/bin/r.rs
@@ -1,33 +1,39 @@
-use r126::gate::send;
+use std::time::Duration;
+
+use r126::gate::{buy, sell, send};
+use tokio::time::Instant;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
-    tokio::spawn(async move {
-        println!("move");
-        let bought = false;
-        while !bought {
-            println!("buy");
-            
-            match send(
-                "spot/orders",
-                &serde_json::json!({
-                    "currency_pair": "NC_USDT",
-                    "type": "market",
-                    "side": "buy",
-                    "account": "spot",
-                    "amount": 1.44,
-                }),
-            )
-            .await
-            {
-                Ok(r) => println!("buy res: {:#?}", r),
-                Err(e) => println!("buy error: {:#?}", e),
-            };
-        }
-    })
-    .await?;
+    // buy("CAM", 3.0).await;
+    sell("CAM", 144.0).await;
+    
+    // tokio::spawn(async move {
+    //     // let bought = false;
+    //     let mut count = 0;
+    //     let start = Instant::now();
+    //     while start.elapsed() < Duration::from_secs(1) {
+    //         buy("CAM", 1.44).await;
+    //         count += 1;
+    //     }
+    //     println!("buy count: {count}");
+    //     println!("buy time: {}", start.elapsed().as_secs());
+    // })
+    // .await?;
 
-    dotenv::dotenv().ok();
+    // tokio::spawn(async move {
+    //     let sold = false;
+    //     // let mut count = 0;
+    //     // let start = Instant::now();
+    //     while !sold {
+    //         // while start.elapsed() < Duration::from_secs(1) {
+    //         sell("CAM", 1.44).await;
+    //         // count += 1;
+    //     }
+    //     // println!("sell count: {count}");
+    //     // println!("sell time: {}", start.elapsed().as_secs());
+    // })
+    // .await?;
     Ok(())
 }
--- src/gate.rs
+++ src/gate.rs
@@ -1,15 +1,40 @@
+use serde::Deserialize;
 use serde::Serialize;
 use serde_json::json;
 use sha2::Digest;
 use sha2::Sha512;
 use std::env;
 
-use hmac::{Hmac, Mac};
 use hex::encode;
+use hmac::{Hmac, Mac};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 // Define type alias for Hmac-Sha512
 type HmacSha512 = Hmac<Sha512>;
+
+enum TradeStatus {
+    Untradable,
+    Buyable,
+    Sellable,
+    Tradable,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Pair {
+    id: Option<String>,
+    base: Option<String>,
+    quote: Option<String>,
+    fee: Option<String>,
+    min_base_amount: Option<String>,
+    min_quote_amount: Option<String>,
+    max_base_amount: Option<String>,
+    max_quote_amount: Option<String>,
+    amount_precision: Option<u32>,
+    precision: Option<u32>,
+    trade_status: Option<TradeStatus>,
+    sell_start: Option<i64>,
+    buy_start: Option<i64>,
+}
 
 fn generate_signature(
     method: &str,
@@ -40,7 +65,6 @@ fn generate_signature(
 }
 
 pub async fn send<T: Serialize>(path: &str, payload: &T) -> anyhow::Result<serde_json::Value> {
-    println!("send");
     let key = env::var("GATE")?;
     let secret = env::var("GATE_SECRET")?;
     let method = "POST";
@@ -51,11 +75,9 @@ pub async fn send<T: Serialize>(path: &str, payload: &T) -> anyhow::Result<serde
         .duration_since(UNIX_EPOCH)?
         .as_secs()
         .to_string();
-    println!("here");
 
     // Generate payload hash
     let payload_hash = encode(Sha512::digest(payload.as_bytes()));
-    println!("here2");
 
     // Generate the signature
     let signature = generate_signature(
@@ -66,13 +88,13 @@ pub async fn send<T: Serialize>(path: &str, payload: &T) -> anyhow::Result<serde
         &timestamp,
         &secret,
     );
-    println!("here3");
 
     // Create request client and set headers
     let client = reqwest::Client::new();
     let response = client
         .post(format!("https://api.gateio.ws{url}"))
         .header("KEY", key)
+        .header("Content-Type", "application/json")
         .header("Timestamp", &timestamp)
         .header("SIGN", &signature)
         .body(payload)
@@ -80,9 +102,99 @@ pub async fn send<T: Serialize>(path: &str, payload: &T) -> anyhow::Result<serde
         .await?
         .json()
         .await?;
-    println!("here4");
-    
-    println!("response: {:#?}", response);
 
     Ok(response)
+}
+
+pub async fn send_get(path: &str) -> anyhow::Result<serde_json::Value> {
+    let key = env::var("GATE")?;
+    let secret = env::var("GATE_SECRET")?;
+    let method = "POST";
+    let url = format!("/api/v4/{path}");
+    let query_string = "";
+    let payload = ""; // Payload should be hex-encoded SHA512 hash of the request body
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)?
+        .as_secs()
+        .to_string();
+
+    // Generate payload hash
+    let payload_hash = encode(Sha512::digest(payload.as_bytes()));
+
+    // Generate the signature
+    let signature = generate_signature(
+        method,
+        &url,
+        query_string,
+        &payload_hash,
+        &timestamp,
+        &secret,
+    );
+
+    // Create request client and set headers
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("https://api.gateio.ws{url}"))
+        .header("KEY", key)
+        .header("Content-Type", "application/json")
+        .header("Timestamp", &timestamp)
+        .header("SIGN", &signature)
+        .body(payload)
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(response)
+}
+
+pub async fn sell(t: &str, amount: f64) {
+    match send(
+        "spot/orders",
+        &serde_json::json!({
+            "currency_pair": t.to_owned() + "_USDT",
+            "type": "market",
+            "side": "sell",
+            "time_in_force": "fok",
+            "account": "spot",
+            "amount": amount,
+        }),
+    )
+    .await
+    {
+        Ok(r) => {
+            println!("sold {amount} {t}. res: {:#?}", r);
+            // sold = true;
+        }
+        Err(e) => println!("sell error: {:#?}", e),
+    };
+}
+
+pub async fn buy(t: &str, amount: f64) {
+    match send(
+        "spot/orders",
+        &serde_json::json!({
+            "currency_pair": "SOL_USDT",
+            "type": "market",
+            "side": "buy",
+            "time_in_force": "ioc",
+            "account": "spot",
+            "amount": amount,
+            // "price":  0.144
+        }),
+    )
+    .await
+    {
+        Ok(r) => {
+            println!("bought {amount} {t}. res: {:#?}", r);
+            // bought = true;
+        }
+        Err(e) => println!("buy error: {:#?}", e),
+    };
+}
+
+pub async fn pairs() -> anyhow::Result<Vec<Pair>> {
+    Ok(serde_json::from_value::<Vec<Pair>>(
+        send_get("/spot/currency_pairs").await?,
+    )?)
 }
